89c0a4aaf6fa53296df7d330101cadf475a4c6ae
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-16 18:00:29 +0100

    added container and Header in App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
+import Container from "./common/Container";
+import { Header } from "./common/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container>
+    <Header>lalala</Header>
+    </Container>
+    nanana</>
   );
 }
 
