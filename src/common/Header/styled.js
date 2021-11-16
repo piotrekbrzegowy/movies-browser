13fef8246b7e118bdb882b0ffbf36af755615261
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-16 17:59:44 +0100

    added styles to Header

---
+++ src/common/Header/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.header`
+    background-color: #18181B;
+    height: 94px;
+    padding: 0px 292px;
+
+`;
+
+export const Stroke1 = styled.div`
+    padding-left: 5px;
+    border: 2.5px solid #FFFFFF;
+    height: 23.15166664123535px;
+    width: 22.295000076293945px;
+    margin-left: 4.58349609375px;
+    margin-top: 8.74969482421875px;
+    border-radius: 0px;
+`;
