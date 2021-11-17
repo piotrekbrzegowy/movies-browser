0d382d0f441cd4d8278c0908e159e42c10a61890
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-17 01:50:48 +0100

    MovieList component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Container from "./common/Container";
 import { Header } from "./common/Header";
+import { MovieList } from "./features/MovieList";
 
 function App() {
   return (
     <>
     <Container>
     <Header>lalala</Header>
+    <MovieList></MovieList>
     </Container>
     nanana</>
   );
