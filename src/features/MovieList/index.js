cbd687af472ccd391c55b8d8ccb33259aa1b74a3
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-17 01:53:00 +0100

    MovieList component added - template - only styled

---
+++ src/features/MovieList/index.js
@@ -0,0 +1,10 @@
+import { Container } from "./Container";
+import { Tile } from "./Tile";
+
+export function MovieList() {
+    return (
+        <Container>
+            <Tile></Tile>
+        </Container>
+    );
+};
