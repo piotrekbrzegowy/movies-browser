308c5fe02d1bb53fc44141e5cbd9c9e2021490ec
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-19 17:37:52 +0100

    added Search, Menu and Tittle in Header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,9 +1,43 @@
-import { Stroke1, Wrapper } from "./styled";
+import {
+    Wrapper,
+    StyledHeader,
+    TittleWrapper,
+    Search,
+    SearchWrapper,
+    PageTittle,
+    TittlePicture,
+    Menu,
+    MenuItem,
+    SearchIcon,
+    Img,
+    Input,
+} from "./styled";
 
 export const Header = () => {
     return (
         <Wrapper>
-        <Stroke1></Stroke1>
+            <StyledHeader>
+                <TittleWrapper>
+                    <TittlePicture>
+                        <img src='https://i.postimg.cc/pdVjVLc2/Video.png' alt="camera picture" />
+                    </TittlePicture>
+                    <PageTittle>Movies Browser</PageTittle>
+                </TittleWrapper>
+                <Menu>
+                    <MenuItem>
+                        movies
+                    </MenuItem>
+                    <MenuItem>
+                        people
+                    </MenuItem>
+                </Menu>
+                <SearchWrapper>
+                    <Search>
+                        <SearchIcon><Img src='https://i.postimg.cc/rytqwB4R/Search.png' alt='magnifier' /></SearchIcon>
+                        <Input placeholder="Search for movies..." />
+                    </Search>
+                </SearchWrapper>
+            </StyledHeader>
         </Wrapper>
     )
 };
