b4142985fdbb5a2fc752203159765df22c01a515
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-20 11:14:25 +0100

    Tags component added

---
+++ src/common/Tags/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const TagList = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+`;
+
+export const Tag = styled.li`
+    font-size: 14px;
+    background-color: #E4E6F0;
+    padding: 8px 16px;
+    border-radius: 5px;
+`;
