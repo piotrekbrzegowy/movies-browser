4c79fa4c664b3e34d0ec3f4cea0486b5da3dd210
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-21 20:19:54 +0100

    added Header component in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { MovieList } from "./features/MovieList/";
+import { Header } from "./common/Header/"
 
 function App() {
   return <div className="App">
+    <Header></Header>
     <MovieList></MovieList>
   </div>;
 }
