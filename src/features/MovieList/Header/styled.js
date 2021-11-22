10f3f1a5b9a64d54bf972ad92f918851b16c3cfa
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-22 13:31:18 +0100

    create styles for Header component

---
+++ src/features/MovieList/Header/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.h2` 
+width: 285px;
+height: 43px;
+margin: 56px 0px 24px;
+font-weight: 600;
+font-size: 36px;
+line-height: 120%;
+color: ${({ theme }) => theme.color.woodsmoke};
+text-transform: capitalize;
+`;
