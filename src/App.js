e2e86b5350e4c8f63232623055099cb9a839fe69
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-23 17:35:45 +0100

    HashRouter in App.js added!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { MovieList } from "./features/MovieList/";
-import { Header } from "./common/Header/"
-
+import { Header } from "./common/Header/";
 function App() {
   return <div className="App">
-    <Header></Header>
-    <MovieList></MovieList>
+    <HashRouter>
+      <nav>
+        <Header />
+        <Switch>
+          <Route path="/filmy">
+            <MovieList />
+          </Route>
+          <Route path="/">
+            <Redirect to="/filmy" />
+          </Route>
+        </Switch>
+      </nav>
+    </HashRouter>
   </div>;
-}
+};
 
 export default App;
