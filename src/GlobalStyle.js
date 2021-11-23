e8d9f6a35b4897d4170d33c5decf07b626c76823
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-11-23 17:35:57 +0100

    added styles for NavLink in GlobalStyle.js

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,4 +1,6 @@
 import { createGlobalStyle } from "styled-components";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
@@ -14,4 +16,15 @@ body {
   background-color: ${({ theme }) => theme.color.mercury};
   font-family: 'Poppins', sans-serif;
 }
+`;
+
+export const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+    color: white;
+    
+    &.active {
+      border: 1px solid #FFFFFF;
+      border-radius: 24px;
+      padding: 8px 12px;
+    };
 `;
