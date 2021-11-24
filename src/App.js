989acbda7cffe5c5c8c2342496910cf24feee641
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-24 21:30:59 +0100

    api test connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { MovieList } from "./features/MovieList/";
 import { Header } from "./common/Header/";
+import { apiConnect } from "./common/apiConnect";
 
 function App() {
+  apiConnect("https://api.themoviedb.org/3/movie/popular?api_key=54628f6d7df0fa35378bd39ea74a55d1&language=en-US&page=1");
+
   return <div className="App">
     <HashRouter>
       <nav>
