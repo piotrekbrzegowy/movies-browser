e5fe68c65972149c27bb34e64d97bb8fb2c5abf3
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-27 23:11:19 +0100

    release date - displaying only year

--- src/common/tiles/MovieTile/index.js
+++ src/common/tiles/MovieTile/index.js
@@ -1,15 +1,14 @@
 import { Wrapper, Image, Content, DetailsWrapper, Title, Subtitle } from "./styled";
 
-export function MovieTile({ title, subtitle, poster_path }) {
+export function MovieTile({ title, subtitle, poster_path}) {
     return (
         <Wrapper>
-            <Image src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : "no image"} alt="poster description" />
+            <Image src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : "no image"} alt={`${title} poster`} />
             <Content>
                 <DetailsWrapper>
                     <Title>{title}</Title>
-                    <Subtitle>{subtitle}</Subtitle>
+                    <Subtitle>{subtitle.slice(0, 4)}</Subtitle>
                 </DetailsWrapper>
-
             </Content>
         </Wrapper>
     );
