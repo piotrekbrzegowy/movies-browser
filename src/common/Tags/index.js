b1c6c03b9652d03c88a73879b978b23732aa6e73
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-27 23:08:50 +0100

    tag key changed to genre_ids

--- src/common/Tags/index.js
+++ src/common/Tags/index.js
@@ -1,9 +1,9 @@
 import { TagList, Tag } from "./styled";
 
-export const Tags = ({ tags }) => (
+export const Tags = ({ tags, genre_ids }) => (
     <TagList>
         {tags.map(tag => {
-            return <Tag key={tag}>{tag}</Tag>
+            return <Tag key={genre_ids}>{tag}</Tag>
         })}
     </TagList>
 );
