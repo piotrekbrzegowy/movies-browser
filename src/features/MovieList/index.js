46e253d4ce51ed347f1dce56707666c718f20dd5
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-28 13:38:28 +0100

    console log deleted

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -13,7 +13,7 @@ export function MovieList() {
     useEffect(() => {
         dispatch(fetchMovieList());
     }, []);
-    console.log(results);
+    
     return (
         <>
             <Container>
