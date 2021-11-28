7b3925e69121371e29e39f68dffcd86b9e0966fa
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-11-28 12:24:46 +0100

    unnecessary variable deleted

--- src/common/apiConnect.js
+++ src/common/apiConnect.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const path = "https://api.themoviedb.org/3/movie/popular?api_key=54628f6d7df0fa35378bd39ea74a55d1&language=en-US&page=1";
-
 export const apiConnect = (path) => {
     return axios.get(path)
     .then(response => response.data)
