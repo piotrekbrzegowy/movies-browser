fd5556c100cd6b5fbe20677ca5008419c1b1a45b
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-12-03 19:09:39 +0100

    added route path to MoviePage /movie-page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { MovieList } from "./features/MovieList/";
 import { Header } from "./common/Header/";
+import { MoviePage } from "./features/MoviePage";
 
 function App() {
 
   return <div className="App">
-      <HashRouter>
-        <nav>
-          <Header />
-          <Switch>
-            <Route path="/movies">
-              <MovieList />
-            </Route>
-            <Route path="/">
-              <Redirect to="/movies" />
-            </Route>
-          </Switch>
-        </nav>
-      </HashRouter>
+    <HashRouter>
+      <nav>
+        <Header />
+        <Switch>
+          <Route path="/movie-page">
+            <MoviePage />
+          </Route>
+          <Route path="/movies">
+            <MovieList />
+          </Route>
+          <Route path="/">
+            <Redirect to="/movies" />
+          </Route>
+        </Switch>
+      </nav>
+    </HashRouter>
   </div>;
 };
 
