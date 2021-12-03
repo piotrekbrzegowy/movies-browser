c76e3b8525771f4e489815bac4720b093477f2ee
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-12-03 19:10:37 +0100

    Rating component styled for MoviePage

--- src/common/Rating/index.js
+++ src/common/Rating/index.js
@@ -1,12 +1,12 @@
-import { RatingWrapper, Rate, Votes, StarIcon } from "./styled";
+import { RatingWrapper, Rate, Votes, StarIcon, RateSpan, RateDiv } from "./styled";
 import star from "./icons/star.svg";
 
-export function Rating({ rate, votes }) {
+export function Rating({ rate, votes, moviePage }) {
     return (
-        <RatingWrapper>
-            <StarIcon src={star} />
-            <Rate>{rate}</Rate>
-            <Votes>{votes} votes</Votes>
+        <RatingWrapper moviePage={moviePage}>
+           <RateDiv moviePage={moviePage}><StarIcon moviePage={moviePage} src={star} />
+            <Rate moviePage={moviePage}>{rate}{moviePage && <RateSpan> / 10</RateSpan>}</Rate></RateDiv>
+            <Votes moviePage={moviePage}>{votes} votes</Votes>
         </RatingWrapper>
     );
 };
--- src/common/Rating/styled.js
+++ src/common/Rating/styled.js
@@ -1,10 +1,24 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const RatingWrapper = styled.div`
     display: flex;
     align-items: flex-end;
     gap: 12px;
     margin-top: 10px;
+
+    ${(props) => props.moviePage && css`
+        margin: 0 0 56px 16px;
+        flex-direction: column;
+        align-items: flex-start;
+    `}
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+        ${(props) => props.moviePage && css`
+            margin: 0 0 4px 16px;
+            flex-direction: unset;
+            align-items: flex-end;
+        `}
+    };
 `;
 
 export const Rate = styled.div`
@@ -12,8 +26,27 @@ export const Rate = styled.div`
     font-size: 16px;
     color: ${({ theme }) => theme.color.woodsmoke};
 
+    ${(props) => props.moviePage && css`
+        color: ${({ theme }) => theme.color.white};
+        font-size: 30px;
+        font-weight: 500;
+    `}
+
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
         font-size: 13px;
+
+        ${(props) => props.moviePage && css`
+            font-size: 14px;
+        `}
+    };
+`;
+
+export const RateSpan = styled.span`
+    font-size: 16px;
+    font-weight: 400;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
+        font-size: 10px;
     };
 `;
 
@@ -21,15 +54,38 @@ export const Votes = styled.div`
     font-size: 16px;
     color: ${({ theme }) => theme.color.waterloo};
 
+    ${(props) => props.moviePage && css`
+        color: ${({ theme }) => theme.color.white};
+    `}
+
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
         font-size: 13px;
+
+        ${(props) => props.moviePage && css`
+            font-size: 10px;
+    `}
     };
 `;
 
 export const StarIcon = styled.img`
     width: 24px;
 
+    ${(props) => props.moviePage && css`
+        width: 40px;
+    `}
+
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}){
         width: 16px;
     };
+`;
+
+export const RateDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    align-items: flex-end;
+
+    ${(props) => props.moviePage && css`
+        gap: 8px;
+    `}
 `;
