17b498cfe15f6b23968bde2a69062fafb3820d2a
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-03 04:43:50 +0100

    Container Pagination checker added to peoplelist

--- src/features/PeopleList/index.js
+++ src/features/PeopleList/index.js
@@ -1,7 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCommon, selectError, selectLoading } from "../../common/commonSlice";
+import Container from "../../common/Container";
+import { Pagination } from "../../common/Pagination";
+import { StateChecker } from "../../common/StateChecker";
+import { selectAllPages, selectCurrentPage } from "../MovieList/movieListSlice";
+
 export const PeopleList = () => {
+    const dispatch = useDispatch();
+    const isLoading = useSelector(selectLoading);
+    const isError = useSelector(selectError);
+    const currentPage = useSelector(selectCurrentPage);
+    const allPages = useSelector(selectAllPages);
+
+    useEffect(() => {
+        dispatch(fetchCommon());
+      }, []);
     return (
         <>
-        nanan
+            <Container>
+                <StateChecker isLoading={isLoading} isError={isError}>
+                    <>nanana</>
+                    <Pagination currentPage={currentPage} allPages={allPages} />
+                </StateChecker>
+            </Container>
         </>
     );
 };
