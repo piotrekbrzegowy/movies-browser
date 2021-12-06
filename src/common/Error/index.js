a8eb6409c99fc628f7f8213a2e35b94bdd4c372a
Dominik Blak <bldominikak@gmial.com>
2021-12-06 12:16:51 +0100

    desktop view

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -1,10 +1,15 @@
 import { ReactComponent as DangerIcon } from "./images/danger.svg";
-import { ErrorPageContainer } from "./styled";
+import { ErrorPageContainer, ErrorHeader, ErrorMessage, ErrorButton } from "./styled";
 
 export const ErrorPage = () => {
   return (
     <ErrorPageContainer>
       <DangerIcon />
+      <ErrorHeader>Ooops! Something went wrong...</ErrorHeader>
+      <ErrorMessage>
+        Please check your network connection <br /> and try again
+      </ErrorMessage>
+      <ErrorButton to="/movies">Back to homepage</ErrorButton>
     </ErrorPageContainer>
   );
 };
--- src/common/Error/styled.js
+++ src/common/Error/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const ErrorPageContainer = styled.div`
   display: flex;
@@ -6,9 +7,28 @@ export const ErrorPageContainer = styled.div`
   text-align: center;
   align-items: center;
   justify-content: center;
+  margin-top: 195px;
 `;
 export const ErrorHeader = styled.h1`
-  margin: 0 auto;
+  margin-top: 38px;
   font-weight: 600;
   font-size: 36px;
+  color: ${({ theme }) => theme.color.woodsmoke};
+`;
+export const ErrorMessage = styled.p`
+  font-weight: 500;
+  font-size: 22px;
+  color: ${({ theme }) => theme.color.woodsmoke};
+  margin: 24px auto;
+`;
+export const ErrorButton = styled(Link)`
+  background-color: ${({ theme }) => theme.color.scienceblue};
+  color: ${({ theme }) => theme.color.white};
+  font-weight: 700;
+  font-size: 14px;
+  border-radius: 5px;
+  text-decoration: none;
+  border: none;
+  padding: 16px 24px;
+  margin-top: 24px;
 `;
