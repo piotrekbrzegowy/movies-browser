a32b6bf985bb95a81a31d615f49f80dda52b8c78
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-06 13:47:33 +0100

    added media to peopleTile

--- src/common/tiles/PeopleTile/styled.js
+++ src/common/tiles/PeopleTile/styled.js
@@ -2,19 +2,36 @@ import styled from "styled-components";
 import { Image, Title, Wrapper } from "../MovieTile/styled";
 
 export const Container = styled(Wrapper)`
-    max-width: 208px;
+    width: 208px;
     min-height: 339px;
     padding-bottom: 22px;
+    
+@media(max-width:${({ theme }) => theme.breakpoint.mobileMax}){
+    flex-direction: column;
+    max-width: 136px;
+    min-height: 245px;
+    padding: 16px;
+}
 `;
 
 export const Picture  = styled(Image)`
     max-width: 176px;
     max-height: 264px;
     border-radius: 5px;
+
+    @media(max-width:${({ theme }) => theme.breakpoint.tabletMax}){
+    max-width: 120px;
+    max-height: 178px;
+}
 `;
 
 export const Header = styled(Title)`
-    min-height: 58px;
     margin: auto;
     margin-bottom: 22px;
+
+    @media(max-width:${({ theme }) => theme.breakpoint.tabletMax}){
+    margin-bottom: 0px;
+    font-size: 14px;
+    line-height: 130%;
+}
 `;
