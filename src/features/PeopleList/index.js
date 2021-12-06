537e8b028605f7337f2ed070bc8bdca4ac40f753
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-06 11:49:40 +0100

    refactoring, added toggle page in PeopleList

--- src/features/PeopleList/index.js
+++ src/features/PeopleList/index.js
@@ -1,16 +1,19 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCommon, selectError, selectLoading } from "../../common/commonSlice";
-import Container from "../../common/Container";
 import { TilesList } from "../../common/TilesList";
 import { Pagination } from "../../common/Pagination";
 import { StateChecker } from "../../common/StateChecker";
 import { Subtitle } from "../../common/Subtitle";
 import { PeopleTile } from "../../common/tiles/PeopleTile";
-import { fetchPeopleList, selectPeopleList, selectPeoplesByQuery } from "./peopleListSlice";
+import { fetchPeopleList, resetStatePeopleList, selectPeoplesByQuery } from "./peopleListSlice";
 import { Header } from "../../common/Header";
 import { useQueryParameter } from "../../queryParameters";
+import { startPage } from "../../common/startPage";
+import { useUrlParameter } from "../urlHooks";
 import SearchQueryParamName from "../../common/Header/Search/searchQueryParamName";
+import Container from "../../common/Container";
+
 
 export const PeopleList = () => {
     const dispatch = useDispatch();
@@ -19,11 +22,14 @@ export const PeopleList = () => {
     const isLoading = useSelector(selectLoading);
     const isError = useSelector(selectError);
     const results = useSelector((state) => selectPeoplesByQuery(state, query));
+    const urlPageNumber = +useUrlParameter("page");
+    const page = startPage(urlPageNumber);
 
     useEffect(() => {
         dispatch(fetchCommon());
-        dispatch(fetchPeopleList());
-    }, []);
+        dispatch(fetchPeopleList({ page }));
+        return() => resetStatePeopleList();
+    }, [dispatch, page]);
     return (
         <>
             <StateChecker isLoading={isLoading} isError={isError}>
