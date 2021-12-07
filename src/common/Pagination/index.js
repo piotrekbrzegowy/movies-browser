f823a238238517144aeec77353e5dad420f71424
Dominik Blak <bldominikak@gmial.com>
2021-12-07 18:38:33 +0100

    pagination in movieList

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -11,7 +11,7 @@ export const Pagination = () => {
   const moviesPages = useSelector(selectAllPages);
   const peoplePages = useSelector(selectAllPeoplePage);
   const hashString = window.location.hash;
-  const allPages = hashString === "#/movies" ? moviesPages : peoplePages;
+  const allPages = hashString === "/movies" ? moviesPages : peoplePages;
 
   const intViewportWidth = window.innerWidth;
   const mobileMax = 767;
