9c4288156c99dbaf1047d36de81dcaf9382932c4
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-08 17:49:42 +0100

    added styles to noResult picture

---
+++ src/common/NoResults/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 40px;
+    height: 100%;
+`;
+
+export const Picture = styled.img`
+    width: 668px;
+    height: 509px;
+`;
