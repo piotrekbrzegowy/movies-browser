23f5945dc24bf40b71dc233ad229634a425e5961
Dominik Blak <bldominikak@gmial.com>
2021-12-09 13:09:26 +0100

    styled person Details

--- src/common/tiles/PersonTileDetails/index.js
+++ src/common/tiles/PersonTileDetails/index.js
@@ -1,15 +1,37 @@
-import { Container, PhotoPerson } from "./styled";
+import {
+  Container,
+  PhotoPerson,
+  Title,
+  Description,
+  BirthInfo,
+  Wrapper,
+  BirthdayInfoDetail,
+  Biography,
+} from "./styled";
 
 export const PersonTileDetails = () => {
   return (
     <Container>
       <PhotoPerson />
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-      standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-      type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-      remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-      Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-      of Lorem Ipsum
+      <Description>
+        <Title>Actor Name</Title>
+        <BirthInfo>
+          <Wrapper>
+            <BirthdayInfoDetail>20.03. 1990</BirthdayInfoDetail>
+          </Wrapper>
+          <Wrapper>
+            <BirthdayInfoDetail>Dallas, Texas, USA</BirthdayInfoDetail>
+          </Wrapper>
+        </BirthInfo>
+      </Description>
+      <Biography>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </Biography>
     </Container>
   );
 };
--- src/common/tiles/PersonTileDetails/styled.js
+++ src/common/tiles/PersonTileDetails/styled.js
@@ -8,7 +8,7 @@ export const Container = styled.article`
   gap: 40px;
   row-gap: 24px;
   grid-template-columns: auto 1fr;
-  grid-template-rows: 1fr;
+  grid-template-rows: auto 1fr;
 `;
 export const PhotoPerson = styled.img`
   float: left;
@@ -16,4 +16,58 @@ export const PhotoPerson = styled.img`
   width: 399px;
   height: 564px;
   background-color: green;
+  grid-row: 1 /-1;
+  grid-column: 1;
+`;
+
+export const Description = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr;
+  gap: 24px;
+  grid-row: 1;
+  grid-column: 2/-1;
+`;
+export const Title = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
+  margin: 8px 0 0 0;
+`;
+export const BirthInfo = styled.span`
+  display: grid;
+  align-content: flex-start;
+  gap: 8px;
+`;
+export const Wrapper = styled.span`
+  color: ${({ theme }) => theme.color.waterloo};
+  font-size: 18px;
+  line-height: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 11px 0 0;
+  &:first-child::before {
+    content: "Date of the birth:";
+    padding: 0 11px 0 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+      content: "Birth:";
+      padding: 0 4px 0 0;
+    }
+  }
+  &:nth-child(2)::before {
+    content: "Place of the birth:";
+    padding: 0 11px 0 0;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+      padding: 0 4px 0 0;
+    }
+  }
+`;
+export const BirthdayInfoDetail = styled.article`
+  color: ${({ theme }) => theme.color.black};
+`;
+export const Biography = styled.span`
+  font-size: 20px;
+  line-height: 160%;
+  grid-row: 2 /-1;
+  grid-column: 2 / -1;
 `;
