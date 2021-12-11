4eb75a4a45c04443e5124d7a34cc28b33a4859c7
Dominik Blak <bldominikak@gmial.com>
2021-12-11 13:42:54 +0100

    fetchPersonCreditsSuccess

--- src/features/PersonPage/index.js
+++ src/features/PersonPage/index.js
@@ -1,4 +1,7 @@
+import { useSelector } from "react-redux";
+import { selectPersonCredtis } from "./personSlice";
 import { PersonTileDetails } from "./../../common/tiles/PersonTileDetails";
 export const PersonDetails = () => {
+  const { crew, cast } = useSelector(selectPersonCredtis);
   return <PersonTileDetails />;
 };
--- src/features/PersonPage/personSlice.js
+++ src/features/PersonPage/personSlice.js
@@ -17,6 +17,11 @@ const personSlice = createSlice({
       state.error = false;
       state.person = person;
     },
+    fetchPersonCreditsSuccess: (state, { payload: personCredits }) => {
+      state.loading = false;
+      state.error = false;
+      state.credits = personCredits;
+    },
 
     fetchPersonError: (state) => {
       state.loading = false;
@@ -24,9 +29,9 @@ const personSlice = createSlice({
     },
   },
 });
-export const { fetchPerson, fetchPersonSuccess, fetchPersonError } = personSlice.actions;
+export const { fetchPerson, fetchPersonSuccess, fetchPersonCreditsSuccess, fetchPersonError } = personSlice.actions;
 
 export const selectPersonState = (state) => state.personDetails;
 export const selectPerson = (state) => selectPersonState(state).person;
-
+export const selectPersonCredtis = (state) => selectPersonState(state).credits;
 export default personSlice.reducer;
