509b0ad57bbf7275777191a794d207fcdfbfe487
Piotrek Brzegowy <brzegowypiotrek@gmail.com>
2021-12-16 18:22:38 +0100

    person page props added to styled

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 export const Wrapper = styled.div`
   max-width: 1368px;
   margin: auto;
-  ${({ person }) =>
+  ${( person ) =>
     person &&
     css`
       margin-bottom: 220px;
